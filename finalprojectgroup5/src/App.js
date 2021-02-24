6d1d85be45a5dab6b3cab24210b8b21c55089546
galvarez314 <galvarez@bates.edu>
2021-02-23 20:43:54 -0600

    other resources page

--- finalprojectgroup5/src/App.js
+++ finalprojectgroup5/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Could be something 
-        </p>
-          Learn React real quick only 2 days left
-        <p> Adding another check to make sure i can replicate it when I'm confused
-          later
-        </p>
-
+    <div><h1> Other resources for Covid Information</h1>
+      <a href= "https://www.cdc.gov/coronavirus/2019-ncov/index.html">
+      <h2> CDC National Website Information</h2>
+      </a>
+      <a href = "https://www.maine.gov/dhhs/mecdc/infectious-disease/epi/airborne/coronavirus/index.shtml">
+      <h3> Local Maine Covid Updates </h3>
+      </a>
+      <a href = "https://www.nami.org/Support-Education/NAMI-HelpLine/COVID-19-Information-and-Resources/COVID-19-Resource-and-Information-Guide">
+      <h4> NAMI Covid Information Guide</h4>
+      </a>
     </div>
   );
 }
