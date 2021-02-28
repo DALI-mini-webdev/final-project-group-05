33a9dc8cc9f89459dbb627e963fe062839a219e6
eunahchoi20 <echoi@bates.edu>
2021-02-28 18:28:28 -0500

    testing styling

--- finalprojectgroup5/src/components/TestingInfo.js
+++ finalprojectgroup5/src/components/TestingInfo.js
@@ -1,16 +1,31 @@
 import react, {Component} from 'react';
 import LewistonMap from '../images/LewistonMap.png';
+import './TestingInfo.css';
 
 class TestingInfo extends Component {
     render() {
         return ( 
             <div>
-                <h1>
-                    Testing Information 
+                <h1 className="header">
+                    Testing and Vaccine Information 
                 </h1>
-                <h3> Lewiston COVID-19 Testing Information  </h3>
+                <h3 className="subheader"> Lewiston COVID-19 Testing Information  </h3>
+                <div className="LewistonMap">
+                    <img src={LewistonMap} height="450px" width="600px" alt="map" />
+                </div>
                 <div>
-                    <img src={LewistonMap} height="500px" width="500px" alt="map" />
+                <h2 className="links"> Links</h2>
+                <div>
+                </div>
+                     <a href="https://www.maine.gov/covid19/restartingmaine/keepmainehealthy/testing">
+                <h3 className="linkone"> Click here for Maine Government Site-Covid info</h3>
+                     </a>
+                 <a href="https://www.cmhc.org/about-us/cmh-alert/">
+                 <h3 className="linktwo"> Click Here for Central Maine Healthcare </h3>
+                </a>
+                <a href="https://www.cvs.com/store-locator/cvs-pharmacy-address/8+Union+Street-Auburn-ME-04210/storeid=2382">
+                <h3 className="linkthree"> Click Here for CVS Pharmacy</h3>
+                </a>
                 </div>
             </div>
         )
